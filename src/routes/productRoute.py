3187289b0061fa6e6b2f01a53f2983dac174eeb2
rehansay <sayyadrehan.787@gmail.com>
2026-07-21 23:48:29 +0530

    HTTP Status Codes and Error Handling

--- src/routes/productRoute.py
+++ src/routes/productRoute.py
@@ -1,26 +1,37 @@
-from fastapi import APIRouter
+from fastapi import APIRouter , HTTPException
 from src.utils.utils import get_all_products
 
 productRoutes=APIRouter()
 
 
 @productRoutes.get("/")
-def getAllProducts():
-    return get_all_products()
+def getAllProducts(product_id:int=None):
+    allProducts=get_all_products()
+
+    if not product_id:
+        return allProducts
+    
+    for product in allProducts:
+        if product["id"]==product_id:
+            return product
+    return HTTPException(status_code=400,
+                         detail={"error:Product not found for this ID."})
+
+
+
 
 @productRoutes.get("/{id}")
 def getOneProducts(id:int):
-    print(id)
-    allproducts=get_all_products()
 
-    for product in allproducts:
+    allProducts=get_all_products()
+    for product in allProducts:
         if product["id"]==id:
             return product
+    return HTTPException(status_code=400,
+                         detail={"error:Product not found for this ID."})
 
-
-    return {"message": "Product Not Found"}
 
 
 @productRoutes.post("/create")
 def createNewProducts():
-    return[]
+    return[] 
